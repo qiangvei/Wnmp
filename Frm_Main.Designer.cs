45f30592f547c18cb59619e38d05305197def3cd
qiangvei <qiangvei@hotmail.com>
2019-07-25 18:18:33 +0800

    第一次提交

---
+++ Frm_Main.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Wnmp
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Main));
+            this.UiStart = new System.Windows.Forms.Button();
+            this.UiLogBox = new System.Windows.Forms.RichTextBox();
+            this.UiReload = new System.Windows.Forms.Button();
+            this.UiQuit = new System.Windows.Forms.Button();
+            this.UiStop = new System.Windows.Forms.Button();
+            this.BtnProcesses = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UiStart
+            // 
+            this.UiStart.Location = new System.Drawing.Point(231, 391);
+            this.UiStart.Name = "UiStart";
+            this.UiStart.Size = new System.Drawing.Size(107, 47);
+            this.UiStart.TabIndex = 0;
+            this.UiStart.Text = "启  动";
+            this.UiStart.UseVisualStyleBackColor = true;
+            this.UiStart.Click += new System.EventHandler(this.UiStart_Click);
+            // 
+            // UiLogBox
+            // 
+            this.UiLogBox.Location = new System.Drawing.Point(-1, 3);
+            this.UiLogBox.Name = "UiLogBox";
+            this.UiLogBox.Size = new System.Drawing.Size(800, 370);
+            this.UiLogBox.TabIndex = 1;
+            this.UiLogBox.Text = "";
+            // 
+            // UiReload
+            // 
+            this.UiReload.Location = new System.Drawing.Point(344, 391);
+            this.UiReload.Name = "UiReload";
+            this.UiReload.Size = new System.Drawing.Size(107, 47);
+            this.UiReload.TabIndex = 2;
+            this.UiReload.Text = "重 启";
+            this.UiReload.UseVisualStyleBackColor = true;
+            this.UiReload.Click += new System.EventHandler(this.UiReload_Click);
+            // 
+            // UiQuit
+            // 
+            this.UiQuit.Location = new System.Drawing.Point(457, 391);
+            this.UiQuit.Name = "UiQuit";
+            this.UiQuit.Size = new System.Drawing.Size(107, 47);
+            this.UiQuit.TabIndex = 3;
+            this.UiQuit.Text = "退 出";
+            this.UiQuit.UseVisualStyleBackColor = true;
+            this.UiQuit.Click += new System.EventHandler(this.UiQuit_Click);
+            // 
+            // UiStop
+            // 
+            this.UiStop.Location = new System.Drawing.Point(570, 391);
+            this.UiStop.Name = "UiStop";
+            this.UiStop.Size = new System.Drawing.Size(107, 47);
+            this.UiStop.TabIndex = 4;
+            this.UiStop.Text = "强制退出";
+            this.UiStop.UseVisualStyleBackColor = true;
+            this.UiStop.Click += new System.EventHandler(this.UiStop_Click);
+            // 
+            // BtnProcesses
+            // 
+            this.BtnProcesses.Location = new System.Drawing.Point(684, 391);
+            this.BtnProcesses.Name = "BtnProcesses";
+            this.BtnProcesses.Size = new System.Drawing.Size(107, 47);
+            this.BtnProcesses.TabIndex = 5;
+            this.BtnProcesses.Text = "显示进程";
+            this.BtnProcesses.UseVisualStyleBackColor = true;
+            this.BtnProcesses.Click += new System.EventHandler(this.BtnProcesses_Click);
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnProcesses);
+            this.Controls.Add(this.UiStop);
+            this.Controls.Add(this.UiQuit);
+            this.Controls.Add(this.UiReload);
+            this.Controls.Add(this.UiLogBox);
+            this.Controls.Add(this.UiStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wnmp";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Frm_Main_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UiStart;
+        private System.Windows.Forms.RichTextBox UiLogBox;
+        private System.Windows.Forms.Button UiReload;
+        private System.Windows.Forms.Button UiQuit;
+        private System.Windows.Forms.Button UiStop;
+        private System.Windows.Forms.Button BtnProcesses;
+    }
+}
+
---
+++ Frm_Main.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wnmp
+{
+    public partial class Frm_Main : Form
+    {
+        delegate void UiHandle(Object obj);
+        private Process Php;
+        Thread PhpTh;
+        Thread NginxTh;
+        public Frm_Main()
+        {
+            InitializeComponent();
+            PhpTh = new Thread(RunPhp);
+            NginxTh = new Thread(RunNginx);
+        }
+
+        public void RunPhp()
+        {
+            try
+            {  
+                Php = new Process();
+                Php.StartInfo.UseShellExecute = false;
+                Php.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                Php.StartInfo.FileName = @"D:\wnmp\apps\php\php5.2.17\php-cgi.exe";
+                Php.StartInfo.Arguments = "-b 127.0.0.1:9000 -c php.ini";
+                Php.StartInfo.CreateNoWindow = true;
+                Php.Start();
+                Php.WaitForExit();
+
+            }
+            catch(Exception ex)
+            {
+                Log(ex.Message);
+            }
+        }
+        public void ClosePhp()
+        {
+            try
+            {
+                List<Process> pro = ServiceProcess();
+                foreach (Process p in pro)
+                {
+                    if (p.ProcessName.ToLower() == "php-cgi")
+                    {
+                        if (!p.CloseMainWindow()) { p.Kill(); }
+                    } 
+                }
+            }catch(Exception ex) { Log(ex.Message); }
+        }
+        public void RunNginx()
+        {
+            RunCmd("D:&cd D:/wnmp&nginx.exe", "启动Nginx");//必须先启动，否则后面无限等待 
+        }
+        public void CloseRunNginx()
+        {
+            try
+            {
+                List<Process> pro = ServiceProcess();
+                foreach (Process p in pro)
+                { 
+                    if (p.ProcessName.ToLower() == "nginx")
+                    {
+                        RunCmd("D:&cd D:/wnmp&nginx -s stop");
+                    }
+                }
+            }
+            catch (Exception ex) { Log(ex.Message); }
+        }
+
+        private void UiStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NginxTh.Start();
+                PhpTh.Start();
+            }catch(Exception ex)
+            {
+                Log(ex.Message);
+            }
+        } 
+         
+
+        private void UiReload_Click(object sender, EventArgs e)
+        {
+            RunCmd("D:&cd D:/wnmp&nginx -s reload","重启服务");
+        } 
+        private void UiQuit_Click(object sender, EventArgs e)
+        {
+            CloseService();
+            RunCmd("D:&cd D:/wnmp&nginx -s quit", "退出服务");
+        } 
+
+        private void UiStop_Click(object sender, EventArgs e)
+        {
+            CloseService();
+            RunCmd("D:&cd D:/wnmp&nginx -s stop", "强制退出");
+        }
+
+        private void CloseService()
+        {
+            try
+            {
+                ClosePhp();
+                if (Php != null)
+                {
+                    Php.CloseMainWindow();
+                    Php.Dispose();
+                }
+                PhpTh.Abort();
+                NginxTh.Abort();
+            }catch(Exception ex) { }
+        }
+        public void RunCmd(string cmds)
+        {
+            RunCmd(cmds, "");
+        }
+        public void RunCmd(string cmds , string retMsg)
+        {
+            string outPut = "";
+            string errMsg = "";
+            try
+            { 
+                Process pc = new Process();
+                pc.StartInfo.FileName = "cmd.exe";
+                pc.StartInfo.CreateNoWindow = true;//隐藏窗口运行
+                pc.StartInfo.RedirectStandardInput = true;//重定向输入流
+                pc.StartInfo.RedirectStandardOutput = true;//重定向输出流
+                pc.StartInfo.RedirectStandardError = true;//重定向错误流
+                pc.StartInfo.UseShellExecute = false;
+                pc.Start(); 
+                pc.StandardInput.WriteLine(cmds+"&exit");//输入CMD命令  
+                //pc.StandardInput.WriteLine("exit");//结束执行，很重要的
+                pc.StandardInput.AutoFlush = true;
+                outPut = pc.StandardOutput.ReadToEnd();//读取结果   
+                Log(outPut); //正式使用时不需要把这个信息显示出来
+                errMsg = pc.StandardError.ReadToEnd();//读取结果    
+                if(!string.IsNullOrEmpty(errMsg.Trim()))
+                {
+                    Log("错误信息："+errMsg);
+                } 
+                else
+                {
+                    if (!string.IsNullOrEmpty(retMsg))
+                    {
+                        Log("成功"+ retMsg);
+                    }
+                }
+                pc.WaitForExit();
+                //pc.CloseMainWindow();
+                //pc.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Log(ex.Message);
+            }
+        }
+        public void Log(object obj)
+        {
+            try
+            {
+                if (this.UiLogBox.InvokeRequired)
+                {
+                    UiHandle ui = new UiHandle(Log);
+                    this.UiLogBox.BeginInvoke(ui, new object[] { obj });
+                }
+                else
+                {
+                    this.UiLogBox.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ": " + obj.ToString()+"\r\n"); 
+                }
+            }
+            catch (Exception e)
+            {
+                Log(e.Message);
+            } 
+        } 
+        private void BtnProcesses_Click(object sender, EventArgs e)
+        {
+            Process[] ps = Process.GetProcesses();
+            foreach (Process p in ps)
+            { 
+                try
+                {  
+                    if (p.ProcessName.ToLower() == "php-cgi" || p.ProcessName.ToLower() == "nginx")
+                    {
+                        //if (!p.CloseMainWindow()) { p.Kill(); }
+                        Log(p.Id + "  " + p.ProcessName + "  " + p.MainModule.ModuleName + "  " + p.MainModule.FileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log(ex.Message);
+                } 
+            }
+        }
+        public List<Process> ServiceProcess()
+        {
+            List<Process> pro = new List<Process> { }; 
+            Process[] ps = Process.GetProcesses();
+            foreach (Process p in ps)
+            {
+                try
+                {
+                    if (p.ProcessName.ToLower() == "php-cgi" || p.ProcessName.ToLower()=="nginx")
+                    {
+                        pro.Add(p); 
+                    }
+                }
+                catch (Exception ex){ }
+            }
+            return pro;
+        }
+
+        private void Frm_Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            List<Process> pro = ServiceProcess();
+            foreach (Process p in pro)
+            {
+                //if (p.ProcessName.ToLower() == "php-cgi")
+                //{
+                //    if (!p.CloseMainWindow()) { p.Kill(); }
+                //}
+                //if (p.ProcessName.ToLower() == "nginx")
+                //{
+                //    RunCmd("D:&cd D:/wnmp&nginx -s stop");
+                //}
+                //退出程序后直接结束进程
+                if (!p.CloseMainWindow()) { p.Kill(); }
+            }
+            CloseService();
+        }
+    }
+}
